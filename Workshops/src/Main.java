1c8f91fb27c500505ee8b59706136d0600e0a51a
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2023-12-07 20:13:54 +0200

    Solved first task

--- Workshops/src/Main.java
+++ Workshops/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+    public static void printHallowPattern(int rows, int columns) {
+
+        for (int i = 0; i < rows; i++) {
+
+            if (i == 0 || i == rows - 1) {
+                for (int j = 0; j < columns; j++) {
+                    System.out.print('*');
+                }
+            } else {
+                System.out.print('*');
+                for (int j = 1; j < columns - 1; j++) {
+                    System.out.print(' ');
+                }
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printHallowPattern(10, 30);
     }
 }
