ccee930835a55b47f157e801b56f1338fd38f565
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2023-12-07 20:48:46 +0200

    Solved second task

--- Workshops/src/Main.java
+++ Workshops/src/Main.java
@@ -20,7 +20,16 @@ public class Main {
         }
     }
 
+    public static void printLeftHalfTriangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        printHallowPattern(10, 30);
+        printLeftHalfTriangle(7);
     }
 }
